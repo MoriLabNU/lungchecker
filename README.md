bcf5cb46907fe9d64780cf3c5d5656b01a0f6f2e
Jie QIU <jieqiu@doraemon09.s.m.is.nagoya-u.ac.jp>
2021-12-12 03:15:35 +0900

    init

---
+++ README.md
@@ -0,0 +1,2 @@
+# Usage
+bash fulllung_checker.sh <path to file> / <path to dir>
---
+++ fulllung_checker.sh
@@ -0,0 +1,30 @@
+#! /bin/bash
+time_stamp=$(date +"%Y%m%d_%H%M%S")
+PYTHON=python3
+docker_path=/homes/jieqiu/code_store/NICT/detectlung/
+
+current_path=${PWD}
+
+file=$1
+
+if ! command -v docker &> /dev/null 
+then
+    echo 'docker not exists'
+    exit
+fi
+
+if  [[ "$(docker images -q moda/lungseg 2> /dev/null)" == "" ]];
+then 
+    cd ${docker_path}
+    docker build -f Dockerfile -t moda/lungseg .
+    #echo ${PWD}
+    cd ${current_path}
+fi
+if   [ -d "${file}" ]
+then docker run -it --gpus all --rm --volume ${file}:/data --workdir /usr/src/app --name moda_lung moda/lungseg ${PYTHON} /usr/src/app/lungsegmentation_test_2d.py
+elif [ -f "${current_path}/${file}" ]
+then docker run -it --gpus all --rm --volume ${current_path}/${file}:/data/${file} --workdir /usr/src/app --name moda_lung moda/lungseg ${PYTHON} /usr/src/app/lungsegmentation_test_2d.py
+else echo "please ensure the given arg to be file in current dir or path to a dir"
+    exit
+fi
+#echo 'test2'
